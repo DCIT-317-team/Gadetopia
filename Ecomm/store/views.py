a0d26a5fb102ccb0f693e607ea5eff35507c0faa
JonathanPar-ker <149477535+JonathanPar-ker@users.noreply.github.com>
2023-11-11 21:22:58 +0000

    guest functionality added

--- Ecomm/store/views.py
+++ Ecomm/store/views.py
@@ -3,18 +3,14 @@ from .models import *
 from django.http import JsonResponse
 import json
 import datetime
+from .models import *
+from .utils import cookieCart, cartData
 
 # Create your views here.
 def store(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer= customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0 ,'shipping':False}
-        cartItems = order['get_cart_items']
+    data = cartData(request)
+    cartItems = data['cartItems']
+    
     
     products = Product.objects.all()
     context = {'products':products, 'cartItems':cartItems}
@@ -22,29 +18,21 @@ def store(request):
 
 def cart(request):
     
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer= customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False }
-        cartItems = order['get_cart_items']
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+                
     context={'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'cart.html',context)
 
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer= customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False }
-        cartItems = order['get_cart_items']
+     
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
     context={'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'checkout.html', context)
 
